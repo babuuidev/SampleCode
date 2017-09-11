db5976697c2afa7db31791317a0123d091cb5c5a
babuuidev <31417263+babuuidev@users.noreply.github.com>
2017-09-11 13:32:59 -0500

    intial commit

---
+++ javascripts/app.js
@@ -0,0 +1,26 @@
+var liData = "";
+
+  $.getJSON( "../javascripts/avocado.json", function( json ) {
+    for(var i=0; i < json.length; i++){
+        liData = liData + `<li>
+                <div class="list">
+                <a href="../images/${json[i].img}" data-lightbox="images" data-title="${json[i].name}"><img src="../images/${json[i].img}"></a>
+                <div class="list-description">
+                <h2>${json[i].name}</h2>
+                <p>${json[i].description}</p>
+                </div>
+                </div>
+                </li>`
+    }
+    $("ol").html(liData);
+ });  
+
+
+
+
+
+
+
+
+
+    
