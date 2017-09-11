73e55d04b1b2f20363222241299a7a0bb47075da
babuuidev <31417263+babuuidev@users.noreply.github.com>
2017-09-11 14:05:30 -0500

    images path changed

--- javascripts/app.js
+++ javascripts/app.js
@@ -4,7 +4,7 @@ var liData = "";
     for(var i=0; i < json.length; i++){
         liData = liData + `<li>
                 <div class="list">
-                <a href="../images/${json[i].img}" data-lightbox="images" data-title="${json[i].name}"><img src="../images/${json[i].img}"></a>
+                <a href="/images/${json[i].img}" data-lightbox="images" data-title="${json[i].name}"><img src="/images/${json[i].img}"></a>
                 <div class="list-description">
                 <h2>${json[i].name}</h2>
                 <p>${json[i].description}</p>
